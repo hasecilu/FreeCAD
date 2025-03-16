c665f3e1b5dc87d26a9e19df4dd63ffd74c7def7
hasecilu <hasecilu@tuta.io>
2025-03-15 18:25:50 -0600

    Surface: add translation source file for uploading to Crowdin

---
+++ src/Mod/Surface/Gui/Resources/translations/Surface.ts
@@ -0,0 +1,508 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>CmdBlendCurve</name>
+    <message>
+        <location filename="../../Command.cpp" line="217"/>
+        <source>Surface</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="218"/>
+        <source>Blend Curve</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="219"/>
+        <source>Join two edges with high continuity</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CmdSurfaceCurveOnMesh</name>
+    <message>
+        <location filename="../../Command.cpp" line="179"/>
+        <source>Surface</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="180"/>
+        <source>Curve on mesh...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="181"/>
+        <source>Creates an approximated curve on top of a mesh.
+This command only works with a &apos;mesh&apos; object.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CmdSurfaceCut</name>
+    <message>
+        <location filename="../../Command.cpp" line="55"/>
+        <source>Surface</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="56"/>
+        <source>Surface Cut function</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="57"/>
+        <source>Cuts a shape with another Shape.
+It returns a modified version of the first shape</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CmdSurfaceExtendFace</name>
+    <message>
+        <location filename="../../Command.cpp" line="277"/>
+        <source>Surface</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="278"/>
+        <source>Extend face</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="279"/>
+        <source>Extrapolates the selected face or surface at its boundaries
+with its local U and V parameters.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CmdSurfaceFilling</name>
+    <message>
+        <location filename="../../Command.cpp" line="114"/>
+        <source>Surface</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="115"/>
+        <source>Filling...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="116"/>
+        <source>Creates a surface from a series of picked boundary edges.
+Additionally, the surface may be constrained by non-boundary edges
+and non-boundary vertices.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CmdSurfaceGeomFillSurface</name>
+    <message>
+        <location filename="../../Command.cpp" line="147"/>
+        <source>Surface</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="148"/>
+        <source>Fill boundary curves</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="149"/>
+        <source>Creates a surface from two, three or four boundary edges.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>CmdSurfaceSections</name>
+    <message>
+        <location filename="../../Command.cpp" line="324"/>
+        <source>Surface</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="325"/>
+        <source>Sections...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="326"/>
+        <source>Creates a surface from a series of sectional edges.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Command</name>
+    <message>
+        <location filename="../../Command.cpp" line="129"/>
+        <location filename="../../Command.cpp" line="165"/>
+        <location filename="../../Command.cpp" line="337"/>
+        <source>Create surface</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="245"/>
+        <source>Blend Curve</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="292"/>
+        <source>Extend surface</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../TaskFilling.cpp" line="65"/>
+        <location filename="../../TaskGeomFillSurface.cpp" line="63"/>
+        <source>Edit filling</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskGeomFillSurface.cpp" line="662"/>
+        <source>Surface</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskSections.cpp" line="59"/>
+        <source>Edit sections</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SurfaceGui::FillingEdgePanel</name>
+    <message>
+        <location filename="../../TaskFillingEdge.cpp" line="137"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFillingEdge.cpp" line="310"/>
+        <source>Invalid object</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../../TaskFillingEdge.cpp" line="388"/>
+        <source>Edge has %n adjacent face(s)</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../../TaskFillingEdge.cpp" line="392"/>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFillingEdge.cpp" line="416"/>
+        <source>Edge has no adjacent faces</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SurfaceGui::FillingPanel</name>
+    <message>
+        <location filename="../../TaskFilling.cpp" line="288"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFilling.cpp" line="488"/>
+        <source>Invalid object</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message numerus="yes">
+        <location filename="../../TaskFilling.cpp" line="604"/>
+        <source>Edge has %n adjacent faces</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message>
+        <location filename="../../TaskFilling.cpp" line="608"/>
+        <source>None</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFilling.cpp" line="629"/>
+        <source>Edge has no adjacent faces</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SurfaceGui::FillingVertexPanel</name>
+    <message>
+        <location filename="../../TaskFillingVertex.cpp" line="129"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SurfaceGui::GeomFillSurface</name>
+    <message>
+        <location filename="../../TaskGeomFillSurface.ui" line="20"/>
+        <source>Filling</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskGeomFillSurface.ui" line="26"/>
+        <source>Fill type:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskGeomFillSurface.ui" line="38"/>
+        <source>Stretch</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskGeomFillSurface.ui" line="48"/>
+        <source>Coons</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskGeomFillSurface.ui" line="55"/>
+        <source>Curved</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskGeomFillSurface.ui" line="76"/>
+        <source>Add Edge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskGeomFillSurface.ui" line="95"/>
+        <source>Remove Edge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskGeomFillSurface.cpp" line="220"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskGeomFillSurface.cpp" line="225"/>
+        <source>Flip orientation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskGeomFillSurface.cpp" line="385"/>
+        <source>Too many edges</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskGeomFillSurface.cpp" line="386"/>
+        <location filename="../../TaskGeomFillSurface.cpp" line="392"/>
+        <source>The tool requires two, three or four edges</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskGeomFillSurface.cpp" line="391"/>
+        <source>Too less edges</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskGeomFillSurface.cpp" line="401"/>
+        <source>Invalid object</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SurfaceGui::Sections</name>
+    <message>
+        <location filename="../../TaskSections.ui" line="14"/>
+        <location filename="../../TaskSections.ui" line="24"/>
+        <source>Sectional edges</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskSections.ui" line="20"/>
+        <source>Add the edges that will be sectional cuts of the surface,
+that is, the surface will be forced to pass through these edges.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskSections.ui" line="41"/>
+        <source>Add Edge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskSections.ui" line="60"/>
+        <source>Remove Edge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskSections.ui" line="72"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;List can be reordered by dragging&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SurfaceGui::SectionsPanel</name>
+    <message>
+        <location filename="../../TaskSections.cpp" line="283"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskSections.cpp" line="419"/>
+        <source>Invalid object</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SurfaceGui::TaskFilling</name>
+    <message>
+        <location filename="../../TaskFilling.ui" line="20"/>
+        <source>Boundaries</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFilling.ui" line="28"/>
+        <source>Support surface</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFilling.ui" line="44"/>
+        <source>Add the edges that will limit the surface.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFilling.ui" line="47"/>
+        <source>Boundary edges</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFilling.ui" line="64"/>
+        <source>Add Edge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFilling.ui" line="83"/>
+        <source>Remove Edge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFilling.ui" line="95"/>
+        <source>Drag the items to reorder the list</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFilling.ui" line="111"/>
+        <source>Drag the items to reorder the list.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFilling.ui" line="131"/>
+        <source>Faces:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFilling.ui" line="151"/>
+        <source>Continuity:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFilling.ui" line="184"/>
+        <source>Accept</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFilling.ui" line="194"/>
+        <source>Ignore</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SurfaceGui::TaskFillingEdge</name>
+    <message>
+        <location filename="../../TaskFillingEdge.ui" line="20"/>
+        <source>Edge constraints</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFillingEdge.ui" line="26"/>
+        <source>Add edges that will be used to constrain the surface,
+that is, the surface will be forced to pass through these edges.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFillingEdge.ui" line="30"/>
+        <source>Non-boundary edges</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFillingEdge.ui" line="47"/>
+        <source>Add Edge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFillingEdge.ui" line="66"/>
+        <source>Remove Edge</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFillingEdge.ui" line="94"/>
+        <source>Faces:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFillingEdge.ui" line="114"/>
+        <source>Continuity:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFillingEdge.ui" line="147"/>
+        <source>Accept</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFillingEdge.ui" line="157"/>
+        <source>Ignore</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SurfaceGui::TaskFillingVertex</name>
+    <message>
+        <location filename="../../TaskFillingVertex.ui" line="14"/>
+        <source>Vertex constraints</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFillingVertex.ui" line="20"/>
+        <source>Add vertices that will be used to constrain the surface,
+that is, the surface will be forced to pass through these points.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFillingVertex.ui" line="24"/>
+        <source>Non-boundary vertices</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFillingVertex.ui" line="41"/>
+        <source>Add Vertex</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../TaskFillingVertex.ui" line="60"/>
+        <source>Remove Vertex</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Surface_ExtendFace</name>
+    <message>
+        <location filename="../../Command.cpp" line="308"/>
+        <source>Wrong selection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../Command.cpp" line="309"/>
+        <source>Select a single face</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
